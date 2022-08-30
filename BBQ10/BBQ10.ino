46120c65c3f4558dd727a55be61fbcc230d65ed2
Dakkaron <dak1st@gmx.at>
2022-08-30 22:50:21 +0200

    Many improvements to the firmware source code, including support for different boards and a separate config file

--- BBQ10/BBQ10.ino
+++ BBQ10/BBQ10.ino
@@ -1,61 +1,152 @@
-/*
- * ## Power Saving
- * Without power saving options the whole device consumes around 60mA.
- * While it's active, that's fine, but it consumes the same amount of
- * energy while it's idle. Since the device is idle for most of the time,
- * reducing the idle power is important.
- * 
- * For this there are multiple options:
- * 
- * ### IDLE_TIMEOUT
- * This is the basic setting that determines after how many ms
- * powersaving features are triggered. If the device was idle for more
- * than the given time, the keyboard backlight is disabled and the given
- * power save mode is applied (if any).
- * 
- * ### POWERSAVE_IDLE
- * This is the recommended powersaving mode. It's a light sleep that keeps
- * the USB connection active. It saves around 40mA. It introduces a delay
- * of up to 120ms when the device is woken up.
- * 
- * ### POWERSAVE_POWERDOWN
- * This is the more aggressive powersaving method. It completely turns off
- * the microcontroller, saving around 55mA. This takes the power consumption
- * down to almost nothing, but USB needs to reconnect after the keyboard
- * is woken up. This takes about 1-2 seconds.
- * 
- * You can use either POWERSAVE_IDLE or POWERSAVE_POWERDOWN or none, but not both.
- */
-#define IDLE_TIMEOUT 20L * 1000L
-#define POWERSAVE_IDLE
-//#define POWERSAVE_POWERDOWN
+#define FAIRBERRY_V0_1_1 1
+#define ARDUINO 10
+#define BEETLE 11
+#define ESP32 12
 
 
-/*
- * To visually show that the device is in cursor mode, the keyboard backlight
- * will blink rapidly if BLINK_IN_CURSOR_MODE is set. Change BLINK_TIMEOUT
- * to change the blinking frequency.
- */
-#define BLINK_IN_CURSOR_MODE
-#define BLINK_TIMEOUT 20L
+#define CHIP_ATMEGA32U4 1
+#define CHIP_ESP32 2
 
+#include "configuration.h"
 
-#if defined(POWERSAVE_IDLE) && defined(POWERSAVE_POWERDOWN)
-  #error "Cannot have POWERSAVE_IDLE and POWERSAVE_POWERDOWN at the same time!"
+#if BOARD_TYPE == FAIRBERRY_V0_1_1 || BOARD_TYPE == ARDUINO || BOARD_TYPE==BEETLE
+  #define CHIP_TYPE CHIP_ATMEGA32U4
+#elif BOARD_TYPE == ESP32
+  #define CHIP_ESP32
 #endif
 
+#if defined(POWERSAVE_ARDUINO_IDLE) && defined(POWERSAVE_ARDUINO_POWERDOWN)
+  #error "Cannot have POWERSAVE_ARDUINO_IDLE and POWERSAVE_ARDUINO_POWERDOWN at the same time!"
+#endif
 
-#include "Keyboard.h"
+#if CHIP_TYPE != CHIP_ATMEGA32U4 && defined(POWERSAVE_ARDUINO_IDLE)
+  #error "Only boards with ATMEGA32U4 chips suport POWERSAVE_ARDUINO_IDLE!"
+#endif
 
-#include "LowPower.h"
+#if CHIP_TYPE != CHIP_ATMEGA32U4 && defined(POWERSAVE_ARDUINO_POWERDOWN)
+  #error "Only boards with ATMEGA32U4 chips suport POWERSAVE_ARDUINO_POWERDOWN!"
+#endif
 
-byte rows[] = {9,8,7,6,5,4,A2};
+#if defined(POWERSAVE_ARDUINO_CLOCKDOWN) && !defined(ARDUINO_CLOCKDOWN_DIVISION)
+  #error "ARDUINO_CLOCKDOWN_DIVISION needs to be set (to 1,2,4,8,16,32,64,128 or 256) if POWERSAVE_ARDUINO_CLOCKDOWN is enabled"
+#endif
+
+#ifdef ARDUINO_CLOCKDOWN_DIVISION
+  #if ARDUINO_CLOCKDOWN_DIVISION == 1
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000000
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 2
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000001
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 4
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000010
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 8
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000011
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 16
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000100
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 32
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000101
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 64
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000110
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 128
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00000111
+  #elif ARDUINO_CLOCKDOWN_DIVISION == 256
+    #define ARDUINO_CLOCKDOWN_DIVISION_VALUE 0b00001000
+  #endif
+#endif
+
+#if BOARD_TYPE == FAIRBERRY_V0_1_1
+  #define F_CPU 16000000
+  #define TX_RX_LED_INIT  DDRE |= (1<<6), DDRB |= (1<<0)
+  #define TXLED0      PORTE |= (1<<6)
+  #define TXLED1      PORTE &= ~(1<<6)
+  #define RXLED0      PORTB |= (1<<0)
+  #define RXLED1      PORTB &= ~(1<<0)
+#endif
+
+#if CHIP_TYPE == CHIP_ATMEGA32U4
+  #if !defined(DEBUG_SERIAL_INSTEAD_OF_USB)
+    #include "Keyboard.h"
+  #endif
+  #include "LowPower.h"
+
+  #ifdef DEBUG_SERIAL_INSTEAD_OF_USB
+    #define KEYBOARD_BEGIN(layout) Serial.begin()
+    #define KEYBOARD_BEGIN(layout) ;
+    #define KEYBOARD_PRESS(key) Serial.print("Pressed ");Serial.println(key);
+    #define KEYBOARD_RELEASE(key) Serial.print("Released ");Serial.println(key);
+  #else
+    #define KEYBOARD_BEGIN(layout) Keyboard.begin(layout)
+    #define KEYBOARD_END() Keyboard.end()
+    #define KEYBOARD_PRESS(key) Keyboard.press(key)
+    #define KEYBOARD_RELEASE(key) Keyboard.release(key)
+  #endif
+#elif BOARD_TYPE == ESP32
+  #include "analogWrite.h"
+  #define USE_NIMBLE
+  #include <BleKeyboard.h>
+  #include "esp_bt.h"
+  #include "esp_pm.h"
+  #include "esp_wifi.h"
+  BleKeyboard bleKeyboard;
+
+  #define SLEEP_DURATION 120000L
+  #define KEYBOARD_BEGIN(layout) bleKeyboard.begin()
+  #define KEYBOARD_END() bleKeyboard.end()
+  #define KEYBOARD_PRESS(key) bleKeyboard.press(key)
+  #define KEYBOARD_RELEASE(key) bleKeyboard.release(key)
+#endif
+
+#if BOARD_TYPE == FAIRBERRY_V0_1_1
+  byte rows[] = {A1,A2,8,9,10,5,A0};
+  byte cols[] = {4,30,1,12,6};
+
+  const byte KEYBOARD_LIGHT_PIN = 13;
+  #define LED0_PIN 0
+  #define LED1_PIN 11
+#elif BOARD_TYPE == ARDUINO
+  byte rows[] = {9,8,7,6,5,4,A2};
+  byte cols[] = {A1,A0,15,14,16};
+
+  const byte KEYBOARD_LIGHT_PIN = 10;
+#elif BOARD_TYPE == BEETLE
+  byte rows[] = {A5,11,10,9,18,19,20};
+  byte cols[] = {3,2,0,14,15};
+
+  const byte KEYBOARD_LIGHT_PIN = 10;
+#elif BOARD_TYPE == ESP32
+  #define RESET_PIN 33
+  byte rows[] = {27,25,32,4,0,2,22};
+  byte cols[] = {5,23,19,18,26};
+
+  const byte KEYBOARD_LIGHT_PIN = 35;
+#endif
+
+#ifdef DEBUG_SERIAL_INSTEAD_OF_USB
+  #define KEY_LEFT_ALT '?'
+  #define KEY_LEFT_CTRL '?'
+  #define KEY_LEFT_SHIFT '?'
+  #define KEY_RIGHT_SHIFT '?'
+  #define KEY_RIGHT '?'
+  #define KEY_RETURN '?'
+  #define KEY_BACKSPACE '?'
+  #define KEY_ESC '?'
+  #define KEY_UP_ARROW '?'
+  #define KEY_LEFT_ARROW '?'
+  #define KEY_DOWN_ARROW '?'
+  #define KEY_RIGHT_ARROW '?'
+  #define KEY_TAB '?'
+  #define KEY_F3 '?'
+  #define KEY_F4 '?'
+  #define KEY_F5 '?'
+  #define KEY_F6 '?'
+  #define KEY_F7 '?'
+  #define KEY_F8 '?'
+  #define KEY_F15 '?'
+#endif
+
 const int rowCount = sizeof(rows)/sizeof(rows[0]);
- 
-byte cols[] = {A1,A0,15,14,16};
 const int colCount = sizeof(cols)/sizeof(cols[0]);
 
-const byte KEYBOARD_LIGHT_PIN = 10;
+
 
 bool keys[colCount][rowCount];
 bool lastValue[colCount][rowCount];
@@ -89,7 +180,7 @@ volatile unsigned long idleTimeout = millis() + IDLE_TIMEOUT;
 volatile unsigned long blinkTimeout = millis() + BLINK_TIMEOUT;
 bool blinkState = true;
 bool idleWakeup = false;
-bool usbNeedsReinit = false;
+bool connectionNeedsReinit = false;
 
 bool symbolSelected;
 
@@ -97,6 +188,32 @@ bool keyboardInit = false;
 bool cursorMode = false;
 
 void setup() {
+    #ifdef LED0_PIN
+      pinMode(LED0_PIN, OUTPUT);
+    #endif
+    #ifdef LED1_PIN
+      pinMode(LED1_PIN, OUTPUT);
+    #endif
+    #if BOARD_TYPE == ESP32
+      KEYBOARD_BEGIN(KeyboardLayout_en_US);
+      keyboardInit = true;
+      
+      esp_bt_sleep_enable();
+      
+      esp_pm_config_esp32_t pm_config;
+      pm_config.max_freq_mhz = 80;
+      pm_config.min_freq_mhz = 80;
+      pm_config.light_sleep_enable = true;
+      esp_pm_configure(&pm_config);
+
+      esp_wifi_set_mode(WIFI_MODE_NULL);
+    #endif
+    #ifdef POWERSAVE_ARDUINO_CLOCKDOWN
+      CLKPR = 0b10000000;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+    #endif
     for(int x=0; x<rowCount; x++) {
         //Serial.print(rows[x]); Serial.println(" as input");
         pinMode(rows[x], INPUT);
@@ -197,15 +314,30 @@ void printMatrix() {
           if (keyActive(colIndex, rowIndex)) {
             changeKeyboardBacklight(0, true);
             idleTimeout = millis() + IDLE_TIMEOUT;
-
-            #ifdef POWERSAVE_POWERDOWN
-              if (usbNeedsReinit) {
-                usbNeedsReinit = false;
+            #ifdef POWERSAVE_ARDUINO_CLOCKDOWN
+              CLKPR = 0b10000000;
+              CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+              CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+              CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+            #endif
+            #ifdef POWERSAVE_ARDUINO_POWERDOWN
+              if (connectionNeedsReinit) {
+                connectionNeedsReinit = false;
                 USBDevice.attach();
                 keyboardInit = false;
                 delay(50);
-                Keyboard.begin(KeyboardLayout_en_US);
+                KEYBOARD_BEGIN(KeyboardLayout_en_US);
                 delay(300);
+              }
+            #endif
+            #ifdef POWERSAVE_ESP32_LIGHT_SLEEP
+              if (connectionNeedsReinit) {
+                pinMode(RESET_PIN, OUTPUT);
+                digitalWrite(RESET_PIN, LOW);
+                
+                /*connectionNeedsReinit = false;
+                btStart();
+                KEYBOARD_BEGIN(KeyboardLayout_en_US);*/
               }
             #endif
           }
@@ -238,17 +370,17 @@ void printMatrix() {
             }*/
             if (keyPressed(colIndex, rowIndex)) {
               if (toPrint!=NULL) {
-                Keyboard.press(toPrint);
+                KEYBOARD_PRESS(toPrint);
               }
             } else {
               if (toPrint!=NULL) {
-                Keyboard.release(toPrint);
+                KEYBOARD_RELEASE(toPrint);
               }
               if (other1!=NULL) {
-                Keyboard.release(other1);
+                KEYBOARD_RELEASE(other1);
               }
               if (other2!=NULL) {
-                Keyboard.release(other2);
+                KEYBOARD_RELEASE(other2);
               }
             }
           }
@@ -258,18 +390,18 @@ void printMatrix() {
 
 void loop() {
   idleWakeup = false;
-  readMatrix();
-  
-  printMatrix();
-
-  // key 3,3 is the enter key
-  if (keyPressed(3,3)) {
-    if (!keyboardInit) {
-      Keyboard.begin();
-      //Keyboard.begin(KeyboardLayout_de_DE);
-    }
-    Keyboard.write('\n');
+  unsigned long startms = millis();
+  #if BOARD_TYPE == ESP32
+  if (bleKeyboard.isConnected()) {
+  #endif
+    readMatrix();
+    printMatrix();
+    //Serial.print("matrix: ");
+    //Serial.println(millis()-startms);
+  #if BOARD_TYPE == ESP32
   }
+  #endif
+
   // increase backlight if mic key + sym key is pressed
   if (keyActive(0,6) && keyPressed(0,2)) {
     changeKeyboardBacklight(keyboardLightSteps, true);
@@ -294,17 +426,39 @@ void loop() {
       changeKeyboardBacklight(0, blinkState);
     }
   #endif
+  #ifdef LED0_IN_CURSOR_MODE
+    digitalWrite(LED0_PIN, cursorMode);
+  #endif
+
+  //Serial.print("Loop ");
+  //Serial.println(idleTimeout-millis());
   if (idleTimeout<millis()) {
+    //Serial.println("Sleep");
     changeKeyboardBacklight(0, false);
-    #ifdef POWERSAVE_POWERDOWN
+    #ifdef POWERSAVE_ARDUINO_POWERDOWN
       Keyboard.end();
       USBDevice.detach();
-      usbNeedsReinit = true;
+      connectionNeedsReinit = true;
       LowPower.powerDown(SLEEP_120MS, ADC_OFF, BOD_OFF);
     #endif
-    #ifdef POWERSAVE_IDLEuz
+    #ifdef POWERSAVE_ARDUINO_CLOCKDOWN
+      CLKPR = 0b10000000;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+      CLKPR = ARDUINO_CLOCKDOWN_DIVISION_VALUE;
+    #endif
+    #ifdef POWERSAVE_ARDUINO_IDLE
       LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_ON);
     #endif
+    #ifdef POWERSAVE_ESP32_LIGHT_SLEEP
+      if (!connectionNeedsReinit) {
+        btStop();
+        connectionNeedsReinit = true;
+      }
+      esp_sleep_enable_timer_wakeup(SLEEP_DURATION);
+      esp_light_sleep_start();
+    #endif
   }
-  delay(10);
+  //Serial.print("total:  ");
+  //Serial.println(millis()-startms);
 }
---
+++ BBQ10/configuration.h
@@ -0,0 +1,72 @@
+/*
+ * ## Mainboard type
+ * Can be either of the following:
+ * FAIRBERRY_V0_1_1 : This is the custom mainboard, version 0.1.1
+ * ARDUINO : The original option, using an Arduino Pro Micro
+ * BEETLE : A slightly different version of the original option, using a smaller Beetle board based on the ATMega32u4. Not recommeded, because the beetle has one to few pins, so you have to solder directly to the ATMega32u4.
+ * ESP32: Using an ESP32-based board to couple the board using Bluetooth. Not recommended, because the power options don't really work right now, and it consumes a lot of power.
+ */
+#define BOARD_TYPE FAIRBERRY_V0_1_1
+
+/*
+ * ## Power Saving
+ * Without power saving options the whole device consumes around 60mA.
+ * While it's active, that's fine, but it consumes the same amount of
+ * energy while it's idle. Since the device is idle for most of the time,
+ * reducing the idle power is important.
+ * 
+ * For this there are multiple options:
+ * 
+ * ### IDLE_TIMEOUT
+ * This is the basic setting that determines after how many ms
+ * powersaving features are triggered. If the device was idle for more
+ * than the given time, the keyboard backlight is disabled and the given
+ * power save mode is applied (if any).
+ * 
+ * ### POWERSAVE_ARDUINO_IDLE
+ * This is the recommended powersaving mode for BOARD_TYPE ARDUINO. It's a light sleep that keeps
+ * the USB connection active. It saves around 40mA. It introduces a delay
+ * of up to 120ms when the device is woken up.
+ * 
+ * ### POWERSAVE_ARDUINO_POWERDOWN
+ * This is the more aggressive powersaving method for BOARD_TYPE ARDUINO. It completely turns off
+ * the microcontroller, saving around 55mA. This takes the power consumption
+ * down to almost nothing, but USB needs to reconnect after the keyboard
+ * is woken up. This takes about 1-2 seconds.
+ * 
+ * ### POWERSAVE_ESP32_LOGHT_SLEEP
+ * This is the recommended powersaving mode for BOARD_TYPE ARDUINO. It's a light sleep that keeps
+ * the Bluetooth connection active. It reduces the power consumption from around 160mA to about 20mA when not active.
+ * It introduces a delay of up to 50ms when the device is woken up.
+ * 
+ * For Arduino boards you can use either POWERSAVE_ARDUINO_IDLE or POWERSAVE_ARDUINO_POWERDOWN or none, but not both.
+ * 
+ * For ESP32 boards you can either use POWERSAVE_ESP32_LIGHT_SLEEP or none.
+ */
+#define IDLE_TIMEOUT 60L * 1000L
+#define POWERSAVE_ARDUINO_IDLE
+//#define POWERSAVE_ARDUINO_CLOCKDOWN
+//#define POWERSAVE_ARDUINO_POWERDOWN
+//#define POWERSAVE_ESP32_LIGHT_SLEEP
+//#define POWERSAVE_ESP32_DEEP_SLEEP
+
+//#define ARDUINO_CLOCKDOWN_DIVISION 16
+
+
+/*
+ * To visually show that the device is in cursor mode, the keyboard backlight
+ * will blink rapidly if BLINK_IN_CURSOR_MODE is set. Change BLINK_TIMEOUT
+ * to change the blinking frequency.
+ * 
+ * If you are using a fairberry mainboard, you can use LED0_IN_CURSOR_MDOE
+ * instead of BLINK_IN_CURSOR_MODE, to have the LED0 glow when in cursor mode (doesn't blink).
+ */
+//#define BLINK_IN_CURSOR_MODE
+#define BLINK_TIMEOUT 20L
+#define LED0_IN_CURSOR_MODE
+
+/*
+ * If you define DEBUG_SERIAL_INSTEAD_OF_USB, the device will not function as a keyboard, but instead
+ * output key presses to serial. Used for debugging.
+ */
+//#define DEBUG_SERIAL_INSTEAD_OF_USB
